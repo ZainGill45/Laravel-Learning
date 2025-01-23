53397d32930166ecbdf36dd8157e147e3987d1dd
Zain Gill <zaingill45@gmail.com>
2025-01-23 04:03:31 -0700

    Learned about what factories do and how to generate randomized data through the faker API.

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model {
-    protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
-
+class Employer extends Model
+{
     use HasFactory;
 }
